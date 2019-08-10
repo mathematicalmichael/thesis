ed61c4eb24128a0a343651c4fef701cd867f7533
Michael Pilosov <consistentbayes@gmail.com>
2019-08-10 16:42:57 -0600

    refactor examples structure.

---
+++ images/make_voronoi_diagrams.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python voronoi_unit_domain.py --num 25 --seed 0
+python voronoi_unit_domain.py --num 25 --seed 0 --nolabel
+python voronoi_unit_domain.py --num 25 --seed 10
+python voronoi_unit_domain.py --num 25 --seed 10 --nolabel
+python voronoi_unit_domain.py --num 500 --seed 50
