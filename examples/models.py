02c3d9c1434a5a7c7591f327c751127af30b7420
Michael Pilosov <consistentbayes@gmail.com>
2020-01-12 17:49:19 -0700

    heatrod model validated with animations

--- examples/models.py
+++ examples/models.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import dolfin as df
 
 def makeDecayModel(t):
     """
@@ -151,6 +151,99 @@ def makeSkewModel(skew):
     """
     Q_map = skewmat(skew)
     return makeMatrixModel(Q_map)
+
+
+def make1DHeatModel(temp_locs_list, end_time = 1.0):
+    """
+    TODO: put parameters into options
+    """
+    # heatrod code here - including all settings
+    t_stop = end_time
+
+    # Some fixed parameter values
+    amp = 50.0  # amplitude of the heat source
+    px = 0.5  # location of the heat source
+    width = 0.05  # width of the heat source
+    T_R = 0  # initial temp of the plate
+    cap = 1.5  # heat capacity
+    rho = 1.5  # density
+
+    # 'parameters' reserved for FEnICS
+    df.parameters['allow_extrapolation'] = True
+
+    # mesh properties
+    nx = 50  # this is our h value
+    mesh = df.IntervalMesh(nx, 0, 1)
+    degree = 1
+    r = 1.0  # time stepping ratios - attention to stability
+    dt = r/nx
+
+    # turn off the heat halfway through
+    t_heatoff = 0.5
+
+    def model(parameter_samples):
+        """
+        TODO: string-formatting that displays the models parameters.
+
+        Returns model evaluated at `T={end_time}`.
+        """.format(end_time=end_time)
+
+        QoI_samples = np.zeros((len(parameter_samples), len(temp_locs_list)))
+
+        for i in range(len(parameter_samples)):
+            kappa_0 = parameter_samples[i, 0]
+            kappa_1 = parameter_samples[i, 1]
+
+            # define the subspace we will solve the problem in
+            V = df.FunctionSpace(mesh, 'Lagrange', degree)
+
+            # split the domain down the middle(dif therm cond)
+            kappa_str = 'x[0] > 0.5 ?'\
+                'kappa_1 : kappa_0'
+
+            # Physical parameters
+            kappa = df.Expression(kappa_str, kappa_0=kappa_0,
+                               kappa_1=kappa_1, degree=1)
+            # Define initial condition(initial temp of plate)
+            T_current = df.interpolate(df.Constant(T_R), V)
+
+            # Define variational problem
+            T = df.TrialFunction(V)
+
+            # two f's and L's for heat source on and off
+            f_heat = df.Expression(
+                'amp*exp(-(x[0]-px)*(x[0]-px)/width)', amp=amp, px=px, width=width, degree=1)
+            f_cool = df.Constant(0)
+            v = df.TestFunction(V)
+            a = rho*cap*T*v*df.dx + dt*kappa * \
+                df.inner(df.nabla_grad(v), df.nabla_grad(T))*df.dx
+            L_heat = (rho*cap*T_current*v + dt*f_heat*v)*df.dx
+            L_cool = (rho*cap*T_current*v + dt*f_cool*v)*df.dx
+
+            A = df.assemble(a)
+            b = None  # variable used for memory savings in assemble calls
+
+            T = df.Function(V)
+            t = dt  # initialize first time step
+            print("%d Starting timestepping."%i)
+            # time stepping method is BWD Euler. (theta = 1)
+            while t <= t_stop:
+                if t < t_heatoff:
+                    b = df.assemble(L_heat, tensor=b)
+                else:
+                    b = df.assemble(L_cool, tensor=b)
+                df.solve(A, T.vector(), b)
+
+                t += dt
+                T_current.assign(T)
+
+            # now that the state variable (Temp at time t_stop) has been computed,
+            # we take our point-value measurements
+            QoI_samples[i] = np.array([T(xi) for xi in temp_locs_list])
+        return QoI_samples
+
+    return model
+
 
 
 # this is from another file.
