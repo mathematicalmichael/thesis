5ce288ff59f5ddd0519db748131a934476122ef2
Michael Pilosov <consistentbayes@gmail.com>
2020-08-23 22:42:12 -0600

    touchups to ch2+3

--- examples/heatrod_example.sh
+++ examples/heatrod_example.sh
@@ -15,3 +15,15 @@ M=1
 NUM_SAMPLES=50
 LAM_1=0.15
 LAM_2=0.05
+
+# masters work used 3x3 grid in the interior
+# of (0.01, 0.2)^2 and showed bottom-left + top-center
+
+
+# avg skewness 1.08
+TLOC0=0.25
+TLOC1=0.51
+
+# avg skewness 1.56
+# TLOC0=0.67
+# TLOC1=0.98
